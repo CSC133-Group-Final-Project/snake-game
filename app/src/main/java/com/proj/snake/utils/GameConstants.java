726b1f1b8bb6dd693d4832621beb2b74e1468ed7
Keyur Maru <kd2maru@gmail.com>
2023-11-11 14:58:41 -0800

    Implement AudioManager and Refactor Sound Management in Snake Game
    - Implemented `AudioManagerImpl`, a new class for managing audio functionalities in the Snake game.
    - Integrated the `IAudioManager` interface for consistent audio management across the application.
    - Utilized `SoundPool` for efficient audio playback, supporting simultaneous streams with optimized attributes.
    - Created singleton pattern in `AudioManagerImpl` for global access and resource efficiency.
    - Added sound loading and playing functionalities within `AudioManagerImpl`, handling exceptions and ensuring thread safety.
    - Defined new constants `EAT_SOUND` and `DEATH_SOUND` in `GameConstants` for clear identification of sound effects.
    - Refactored `SnakeGame` to utilize `AudioManagerImpl` for sound effects, replacing direct `SoundPool` usage.
    - Removed commented-out code and unused imports in `SnakeGame` and `ScreenInfo` for cleaner and more maintainable code.
    - Enhanced overall sound management architecture, providing a robust and scalable solution for audio handling in the game.
    
    These changes streamline the audio handling process in the Snake game, ensuring efficient and centralized management of sound resources, thereby improving the maintainability and scalability of the game's audio functionalities.
    
    Signed-off-by: Keyur Maru <kd2maru@gmail.com>

--- app/src/main/java/com/proj/snake/utils/GameConstants.java
+++ app/src/main/java/com/proj/snake/utils/GameConstants.java
@@ -2,4 +2,6 @@ package com.proj.snake.utils;
 
 public class GameConstants {
     public static final int NUM_BLOCKS_WIDE = 40;
+    public static final int EAT_SOUND = 1;
+    public static final int DEATH_SOUND = 2;
 }
